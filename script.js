5d9add16a01e6c4b6f302c2a46735f16c13f86de
pawancodecrafts <kpawanpreet2003@gmail.com>
2025-10-01 12:47:48 +0530

    minor enhanement

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+// access form
 const form = document.getElementById('form');
 const popup = document.getElementById('popup');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    // access form fields with value
     const myname = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const phone = document.getElementById('phone').value.trim();
     const msg = document.getElementById('msg').value.trim();
 
+    // apply basic validation
     if (!myname || !email || !phone || !msg) {
         alert("Please fill all the fields");
         return;
     }
 
-    popup.innerHTML = `Thank you for contacting us, ${myname}! We will get back to you shortly.
-`;
+    //  message after form submission
+    popup.innerHTML = `Thank you for contacting us,<span class="highlight-name"> ${myname}</span> ! We will get back to you shortly.`;
+
+    // add popup + fade in
+    popup.classList.add('popup');
+    setTimeout(() => {
+        popup.classList.add('show')
+    }, 5);
+
+    // clear form after submit
     form.reset();
 });
